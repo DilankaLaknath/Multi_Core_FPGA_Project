302708fc2ef5de6de252a027055e16de284adab7
Dilanka <dilankalaknath97@gail.com>
2021-06-28 00:28:33 +0530

    add register_type_2

---
+++ src/register_type_2.v
@@ -0,0 +1,20 @@
+module Register8
+(
+    input clk,
+    input write_en,
+    input rst_en,
+    input inc_en,
+    input [15:0] data_in,
+    output reg [15:0] data_out
+);
+
+always @ (posedge clk)
+begin
+	if (write_en == 1)
+		data_out <= data_in;
+	if (rst_en == 1)
+		data_out <= 16'd0;
+	if (inc_en == 1)
+		data_out <= data_out + 16'd1;
+end
+endmodule
